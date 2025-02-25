e56c2aa1a2013b648d58d21de176dca10b6c0c5a
GaelGrezz <gutierrezgael838@gmail.com>
2025-02-24 22:11:31 -0500

    The project was finished

---
+++ screens/HomeScreen.js
@@ -0,0 +1,79 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text, Button, FlatList, StyleSheet, Image, ActivityIndicator } from 'react-native';
+
+export default function HomeScreen({ navigation }) {
+  const [characters, setCharacters] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('https://rickandmortyapi.com/api/character')
+      .then(response => response.json())
+      .then(data => {
+        setCharacters(data.results);
+        setLoading(false);
+      })
+      .catch(() => setLoading(false)); // Maneja errores si es necesario
+  }, []);
+
+  if (loading) {
+    return (
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator size="large" color="#0000ff" />
+        <Text>Cargando...</Text>
+      </View>
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={characters}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View style={styles.item}>
+            <Image source={{ uri: item.image }} style={styles.image} />
+            <Text style={styles.name}>{item.name}</Text>
+            <Button
+              title="View Details"
+              onPress={() => navigation.navigate('Detail', { character: item })}
+            />
+          </View>
+        )}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  loadingContainer: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  item: {
+    backgroundColor: '#3a498d',
+    padding: 20,
+    marginVertical: 8,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  image: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    marginRight: 15,
+  },
+  name: {
+    fontSize: 24,
+    color: 'white',
+    fontWeight: 'bold',
+    flex: 1,
+  },
+});
